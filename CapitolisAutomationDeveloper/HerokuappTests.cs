65f94e1d08c7e716dd7ee69f5a973ee1cc73a217
yuri.tsouker@overops.com <yuri.tsouker@overops.com>
2021-06-16 08:05:10 +0300

    Add NLog to proj
    Add NLog to proj

--- CapitolisAutomationDeveloper/HerokuappTests.cs
+++ CapitolisAutomationDeveloper/HerokuappTests.cs
@@ -1,4 +1,5 @@
 using CapitolisAutomationDeveloper.Pages;
+using NLog;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using WebDriverManager;
@@ -8,12 +9,15 @@ namespace CapitolisAutomationDeveloper
 {
     public class Tests : DriverHelper
     {
+        public Logger log = LogManager.GetCurrentClassLogger();
+
         [SetUp]
         public void Setup()
         {
             //Driver = new ChromeDriver(@"C:\chromedriver_win32");
             ChromeOptions option = new ChromeOptions();
             new DriverManager().SetUpDriver(new ChromeConfig());
+            log.Info("Start Chrome Driver");
             Driver = new ChromeDriver(option);
         }
 
@@ -21,6 +25,8 @@ namespace CapitolisAutomationDeveloper
         public void CheckBoxTest()
         {
             HerokuHomePage herokuHomePage = new HerokuHomePage();
+
+            log.Info("Open Home Page");
 
             herokuHomePage.NavigateToHeroku();
 
