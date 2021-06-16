aecc325bf2545a79ba4691e173a640b19b35718d
yuri.tsouker@overops.com <yuri.tsouker@overops.com>
2021-06-16 07:41:13 +0300

    adjusting chrome driver
    adjusting chrome driver

--- CapitolisAutomationDeveloper/HerokuappTests.cs
+++ CapitolisAutomationDeveloper/HerokuappTests.cs
@@ -1,17 +1,20 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
 
 namespace CapitolisAutomationDeveloper
 {
-    public class Tests
+    public class Tests : DriverHelper
     {
         [SetUp]
         public void Setup()
         {
+            Driver = new ChromeDriver(@"C:\chromedriver_win32");
         }
 
         [Test]
         public void Test1()
         {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
             Assert.Pass();
         }
     }
